346e292c5c6d3bfb9e44971f28564840b6564856
KChudzicki <chudzicki.krzysztof1@gmail.com>
2023-06-18 20:07:55 +0200

    test

--- src/main/java/pl/wszib/pizzamarket/web/controllers/CommentsControler.java
+++ src/main/java/pl/wszib/pizzamarket/web/controllers/CommentsControler.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pl.wszib.pizzamarket.services.CommentsService;
 import pl.wszib.pizzamarket.web.models.CommentsModel;
 import pl.wszib.pizzamarket.web.models.CommentsSaveModel;
@@ -12,25 +13,28 @@ import pl.wszib.pizzamarket.web.models.CommentsSaveModel;
 import java.util.List;
 
 @Controller
+@RequestMapping("comments")
 public class CommentsControler {
     private final CommentsService commentsService;
 
-    public CommentsControler(CommentsService commentsService) {
+    private CommentsControler(CommentsService commentsService) {
+
         this.commentsService = commentsService;
     }
 
 
-    @GetMapping("comments")
-    public String CommentsPage(Long commentId,Model model) {
+    @GetMapping
+    public String CommentsPage(Long commentId, Model model) {
         List<CommentsModel> comments = commentsService.findAll();
+        model.addAttribute("coments", comments);
         model.addAttribute("newComment", new CommentsSaveModel());
-//        model.addAttribute("coments", comments);
         return "commentsPage";
     }
-    @PostMapping("comments")
-    public String comment(Long commentsId,@ModelAttribute("comments") CommentsSaveModel commentsSaveModel ){
-       commentsService.saveComment(commentsId,commentsSaveModel);
-               return "commentsPage";
+
+    @PostMapping
+    public String comment(Long commentsId, @ModelAttribute("comments") CommentsSaveModel commentsSaveModel) {
+        commentsService.saveComment(commentsId, commentsSaveModel);
+        return "commentsPage";
     }
 
 }
